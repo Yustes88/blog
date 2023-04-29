40b008182806511dc6f9c8da5885eb0abead7977
Yulya <iuliiaobr.io@gmail.com>
2023-04-29 19:14:33 +0700

    Fix title alignment

--- components/AllPosts.tsx
+++ components/AllPosts.tsx
@@ -25,13 +25,10 @@ const posts = [
 
 export default function AllPosts() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white pb-24 sm:pb-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600">
-            Learn how to grow your business with our expert advice.
-          </p>
+        <div className="mx-auto text-left">
+          <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-4xl">All posts</h2>
         </div>
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post) => (
