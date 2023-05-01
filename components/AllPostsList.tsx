b9e5f0b9126722d0efc5ec8b6efb1ec3287ccee1
Yulya <iuliiaobr.io@gmail.com>
2023-05-01 09:55:55 +0700

    Add filter posts component

--- components/AllPostsList.tsx
+++ components/AllPostsList.tsx
@@ -1,3 +1,5 @@
+'use client'
+import FilterPosts from "./FilterPosts"
 import PostPreview from "./PostPreview"
 
 //TODO fix types
@@ -8,6 +10,9 @@ export default function AllPostsList({posts}: any) {
         <div className="mx-auto text-left">
           <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-4xl">All posts</h2>
         </div>
+
+        <FilterPosts/>
+
         <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           {posts.map((post: any) => (
             <PostPreview post={post.data} key={post.data.id}/>
