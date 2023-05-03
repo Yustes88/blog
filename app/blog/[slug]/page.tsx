4b5520dd7e0f525231e24845e42b3b7fa2227fdf
Yulya <iuliiaobr.io@gmail.com>
2023-05-03 19:52:26 +0700

    Add post global styles

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,8 @@ import fs from "fs";
 import Markdown from "markdown-to-jsx";
 import matter from "gray-matter";
 import getPostMetadata from "@/components/GetPostsData";
+import Header from "@/components/Header";
+import PostFull from "@/components/PostFull";
 
 const getPostContent = (slug: string) => {
   const folder = "posts/";
@@ -23,16 +25,10 @@ const PostPage = (props: any) => {
   const slug = props.params.slug;
   const post = getPostContent(slug);
   return (
-    <div>
-      <div className="my-12 text-center">
-        <h1 className="text-2xl text-slate-600 ">{post.data.title}</h1>
-        <p className="text-slate-400 mt-2">{post.data.date}</p>
-      </div>
-
-      <article className="prose">
-        <Markdown>{post.content}</Markdown>
-      </article>
-    </div>
+    <>
+    <Header/>
+    <PostFull post = {post}/>
+    </>
   );
 };
 
