f9b1192f63b03bd9e64a655281edd7e541ae51cc
Yulya <iuliiaobr.io@gmail.com>
2023-05-06 19:21:57 +0700

    fix top posts size type

--- components/GetPostsData.ts
+++ components/GetPostsData.ts
@@ -29,7 +29,7 @@ export const getPostMetadata = (): Post[] => {
       categoryHref: data.categoryHref,
       imageUrl: data.imageUrl,
       rating: data.rating,
-      size: data.number,
+      size: data.size,
     };
 
     return {
