2b723d7a13d70292b66ee586fa268f329fb40efe
Yulya <iuliiaobr.io@gmail.com>
2023-05-07 19:37:06 +0700

    fix sorting and filter responsive view

--- components/AllPostsList.tsx
+++ components/AllPostsList.tsx
@@ -15,23 +15,11 @@ export default function AllPostsList({posts}: AllPostsListProps) {
   return (
     <div className="bg-white pb-24 sm:pb-32">
       <div className="relative isolate mx-auto max-w-7xl px-6 lg:px-8">
-      {/* <div
-        className="absolute -top-80 left -z-10 transform-gpu blur-3xl sm:left-1/2 md:top-20 lg:ml-20 xl:top-3 xl:ml-56"
-        aria-hidden="true"
-      >
-        <div
-          className="aspect-[801/1036] w-[50.0625rem] bg-gradient-to-tr from-[#adb5bd] to-[#868e96] opacity-30"
-          style={{
-            clipPath:
-              'polygon(63.1% 29.6%, 100% 17.2%, 76.7% 3.1%, 48.4% 0.1%, 44.6% 4.8%, 54.5% 25.4%, 59.8% 49.1%, 55.3% 57.9%, 44.5% 57.3%, 27.8% 48%, 35.1% 81.6%, 0% 97.8%, 39.3% 100%, 35.3% 81.5%, 97.2% 52.8%, 63.1% 29.6%)',
-          }}
-        />
-      </div> */}
         <div className="mx-auto text-left">
           <h2 className="text-xl font-bold tracking-tight text-gray-900 sm:text-4xl">All posts</h2>
         </div>
 
-        <div className="flex justify-between	">
+        <div className="flex justify-between items-baseline	">
           <div className="pt-4">
         <span className="pr-4">Filter:</span><FilterPosts posts = {posts} setBlogPosts={setBlogPosts}/>
           </div>
